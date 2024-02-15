5214fa2d2081dc137385fa298cbe840273275e94
ygursu <yasemingrs@gmail.com>
2024-02-16 01:05:19 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp70/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedengirilenortalamayagorebelge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Lütfen ortalamanızı giriniz");
+            int ortalama = Int32.Parse(Console.ReadLine());
+
+            if (ortalama >= 85)
+            {
+                Console.WriteLine("Takdir Belgesi");
+            }
+            else if (ortalama >= 70)
+            {
+                Console.WriteLine("Teşekkür Belgesi");
+            }
+            else
+            {
+                Console.WriteLine("Hiçbir belge alamadınız");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
